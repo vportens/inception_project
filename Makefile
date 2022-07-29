747040b9eb13fc8a74b02b5c218cf7156a45032d
vportens <viporten@student.42.fr>
2022-07-29 01:00:44 -0400

    add docker compose

---
+++ Makefile
@@ -0,0 +1,28 @@
+DOCKER	=	docker compose
+
+HOME	=	/Users/viporten/
+
+OS		=	$(shell uname)
+
+ifeq ($(OS), Linux)
+	HOME	=	/home/viporten/
+	DOCKER	=	docker-compose
+endif
+
+all		:
+	mkdir -p $(HOME)data/wordpress
+	mkdir -p $(HOME)data/db_data
+	$(DOCKER) -f srcs/docker-compose.yml build
+	$(DOCKER) -f srcs/docker-compose.yml up
+
+stop	:
+	$(DOCKER) -f srcs/docker-compose.yml stop
+
+clean:
+	docker volume ls -qf dangling=true | xargs -r docker volume rm
+	docker system prune -f -a
+
+fclean: stop clean
+	sudo rm -rf ${HOME}data
+
+re : fclean all
--- srcs/requirements/wordpress/config/script.sh
+++ srcs/requirements/wordpress/config/script.sh
@@ -4,8 +4,8 @@ if  [ ! -f "$FILE" ]; then
 	echo "Create config"
 	rm -rf /var/www/html/wp-config.php
 	wp config create --dbname=$DB_NAME --dbuser="wordpress" --dbpass=$WP_PASSWORD --dbhost="mariadb" --path="/var/www/html/" --allow-root --skip-check
-	wp core install --url="localhost" --title="viporten site" --admin_user="viporten" --admin_password="viporten" --admin_email="viporten@student.42.fr" --path="/var/www/html/" --allow-root
-	wp user create bob bob@student.42.fr --role=author --user_pass="bob" --allow-root
+	wp core install --url="localhost" --title="viporten site" --admin_user="viporten" --admin_password="viporten_42" --admin_email="viporten@student.42.fr" --path="/var/www/html/" --allow-root
+	wp user create ajeanne bob@student.42.fr --role=author --user_pass="t" --allow-root
 	touch /var/www/html/.exist
 fi
 echo "Config already done"
